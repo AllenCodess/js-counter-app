86f209ba55345c1b8620e8ddbd46fe5c06510e28
AllenCodess <Aph296@protonmail.com>
2026-01-28 03:20:48 -0500

    gave the button count up functionality

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-// document.querySelector(".count").innerHTML = 3
+
 let count = 0
+
+const countUpBtn = document.getElementById("count-up-btn-el")
+const countEl = document.getElementById("count-el")
+
+countUpBtn.addEventListener("click" , countUp)
+
+function countUp () {
+     count = count + 1;
+     countEl.innerHTML = count
+ 
+}
+
