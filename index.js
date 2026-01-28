c32bd5b236869b4c26bf2b00ceb057703bc6027d
AllenCodess <Aph296@protonmail.com>
2026-01-28 13:24:34 -0500

    created a saving functionalitiy for the site

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ let count = 0
 const countUpBtn = document.getElementById("count-up-btn-el")
 const countDownBtn = document.getElementById("count-down-btn-el")
 const countEl = document.getElementById("count-el")
+const saveCountEl = document.getElementById("save-el")
+const savedEl = document.getElementById("saved-count")
 
 //Event Listeners
 countUpBtn.addEventListener("click" , countUp)
 countDownBtn.addEventListener("click" , countDown)
-
+saveCountEl.addEventListener("click", save )
 
 //Functions
 function countUp () {
@@ -20,5 +22,9 @@ function countUp () {
 function countDown () {
     count -= 1
     countEl.textContent = count
+}
+
+function save () {
+    savedEl.textContent = count
 }
 
